10cf51db4f8ca37941aa6b30e22596aa795d0e51
cmlgnzlz <milogonzm@gmail.com>
2022-04-15 21:42:42 -0400

    Entrega 1

--- src/App.js
+++ src/App.js
@@ -3,23 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Clase 01 / React</h1>
+      <h2>Camilo Gonzalez</h2>
+      <h2>Esto esta escrito dentro de una funcion</h2>
     </div>
-  );
+      );
 }
 
 export default App;
