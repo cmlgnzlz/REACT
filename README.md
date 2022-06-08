ee03873a2eff3210a138f428b26d15f12231fb1c
cmlgnzlz <milogonzm@gmail.com>
2022-06-07 22:21:23 -0400

    variables en env

--- README.md
+++ README.md
@@ -3,11 +3,6 @@
 ## Comision 31860
 
 ### `Camilo Gonzalez`
-### `Clase 11&12`
+### `Proyecto Final`
 
-#### Trabajo en CartWidget, para que el indicador muestre la cantidad de unidades total, ya que anteriormente mostraba la cantidad de items, ignorando la cantidad en particular de cada uno.
-#### Trabajo en App.js, para incorporar las configuraciones de firebase.
-#### Trabajo en ItemDetailContainer, reemplazando async mocks con funcionalidades firebase.
-#### Trabajo en ItemListContainer, reemplazando async mocks con funcionalidades firebase.
-#### Elimina carpeta de imagenes, ya que fueron subidas a un hosting.
-#### Elimina items.js, para utilizar la base de datos creada en Firestore
+#### 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID,
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
